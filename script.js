cae44bc46189075173f66727f1682e2e2cbc81f9
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2025-01-11 17:25:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function toFahrenheit(celsius) {
  // Write your code here
+
+	let fahrenheit = (celsius*9/5)+32
+	return round(fahrenheit)
 }
 
 // Do not change the code below
-const celsius = prompt("Enter Celsius:");
+const celsius = promp("Enter Celsius:");
 alert(toFahrenheit(Number(celsius)));
